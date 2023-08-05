54bc84fdc4d36a56d32838c2035a6e1c934e93d0
AhmadMoshaa <ahmed@holo.ae>
2023-08-05 18:25:05 +0300

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION as "postgres",
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT, 10),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [process.env.TYPEORM_ENTITIES],
+      synchronize: process.env.TYPEORM_SYNCHRONIZE === "true",
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
